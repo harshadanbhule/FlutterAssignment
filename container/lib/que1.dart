5441e635efe3c15bf4232daf75850c46f5a147b8
harshadanbhule007 <harshadanbhule007@gmail.com>
2024-02-13 17:22:01 +0530

    Container

---
+++ container/lib/que1.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Que1 extends StatelessWidget {
+  const Que1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 200,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+}
